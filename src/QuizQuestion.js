ba586613fcbb7b42ce9560a7e9e3a2822ac4c1b2
7Kiba <40463850+7Kiba@users.noreply.github.com>
2018-06-28 10:01:05 +0200

    module5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react' //makes react functions available in th
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+  handleClick (buttonText) {
+    if(buttonText === this.props.quiz_question.answer)
+    {
+      this.props.showNextQuestionHandler();
+    }
+  }
+
   render () {
     return (
       <main>
@@ -12,9 +19,9 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <li>
-              <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
-            </li>
+            {this.props.quiz_question.answer_options.map((answer_option, i) => {
+            return   <QuizQuestionButton key={i} button_text={answer_option}
+                                  clickHandler={this.handleClick.bind(this)}/>})}
           </ul>
         </section>
       </main>
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react' //makes react functions available in this file
 
 class QuizQuestionButton extends Component {
+  handleClick (){
+    this.props.clickHandler(this.props.button_text);
+  }
+
   render () {
     return (
       <li>
-        <button>{this.props.button_text}</button>
+        <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
       </li>
     );
   }
